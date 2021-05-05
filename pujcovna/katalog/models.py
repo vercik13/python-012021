3432c9d64f5d96f47d3549ff5a6c26cc5093c9cd
vercik13 <Mallorry13>
2021-05-05 21:35:53 +0200

    9.lekce

--- pujcovna/katalog/models.py
+++ pujcovna/katalog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Auto(models.Model):
+    registracni_znacka = models.CharField(max_length=100)
+    znacka_vozidla = models.CharField(max_length=100)
+    pocet_kilometru = models.IntegerField()
+    datum_tech_kontroly = models.DateField()
+
+class Zakaznik(models.Model):
+    jmeno = models.CharField(max_length=100)
+    prijmeni = models.CharField(max_length=1000)
+    cislo_ridicskeho_prukazu = models.CharField(max_length=100)
+    datum_narozeni = models.DateField()
+
+class Vypujcka(models.Model):
+    datum_cas_vypujcky = models.DateTimeField()
+    datum_cas_konce_vypujcky = models.DateTimeField()
+    cena_vypujcky = models.IntegerField()
+
+
+
--- pujcovna/katalog/urls.py
+++ pujcovna/katalog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
-    path("", views.SeznamView.as_view())
+path('', views.IndexView.as_view(), name='katalog'),
+path('seznam/', views.SeznamView.as_view(), name='seznam'),
 ]
